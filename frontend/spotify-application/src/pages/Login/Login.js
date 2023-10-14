085953ea3a74f449c3fc169eacf593f83bec4716
Katy Gu <kgu42@gatech.edu>
2023-10-14 02:33:41 -0400

    add scopes value

--- frontend/spotify-application/src/pages/Login/Login.js
+++ frontend/spotify-application/src/pages/Login/Login.js
@@ -6,7 +6,7 @@ const CLIENT_ID = "16ab7919b67d4085ba979a6ec8540a48";
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
 const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/login";
 const SPACE_DELIMETER = "%20";
-const SCOPES = ["user-library-read", "user-top-read", "playlist-modify-public", "user-follow-read"];
+const SCOPES = ["user-library-read", "user-top-read", "playlist-modify-public", "user-follow-read", "playlist-modify-private"];
 const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMETER);
 
 const getReturnedParamsFromSpotifyAuth = (hash) => {
@@ -39,8 +39,8 @@ const Login = () => {
         window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
     }
     return (
-        <div className="container">
-            <h1>hi</h1>
+        <div className="containerLogin">
+            <h1>hkjahskjhskjh</h1>
             <button className="button-27" role="button" onClick={handleLogin}>Login to Spotify</button>
         </div>
     );
