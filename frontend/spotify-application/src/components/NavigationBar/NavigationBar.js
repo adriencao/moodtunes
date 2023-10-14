5476c5658131dc3287c45582625017162426a7cf
Katy Gu <katygu432@gmail.com>
2023-10-14 15:08:49 -0400

    added tabs

--- frontend/spotify-application/src/components/NavigationBar/NavigationBar.js
+++ frontend/spotify-application/src/components/NavigationBar/NavigationBar.js
@@ -1,27 +1,30 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./NavigationBar.css";
 
 const NavigationBar = () => {
     return (
-        <header className="NavigationBar">
-            <nav>
-                <ul>
-                <Link to="/">
-                    <li>Home</li>
-                </Link>
-                <Link to="/upload">
-                    <li>Upload</li>
-                </Link>
-                <Link to="/playlist">
-                    <li>Playlist</li>
-                </Link>
-                </ul>
-            </nav>
-        </header>
+        <div align="center">
+            <img src="NavBar.svg" className="folderTabs"/>
+            <header className="NavigationBar">
+                <nav>
+                    <ul>
+                        <Link to="/">
+                            <li>Home</li>
+                        </Link>
+                        <Link to="/upload">
+                            <li>Upload</li>
+                        </Link>
+                        <Link to="/playlist">
+                            <li>Playlist</li>
+                        </Link>
+                    </ul>
+                </nav>
+            </header>
+        </div>
 
     )
-  }; 
+};
 
 export default NavigationBar;
